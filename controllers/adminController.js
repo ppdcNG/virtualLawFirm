c06071136e1898d9fee259af9030cb19ef4326ab
saslamp <sadiqasg@gmail.com>
2019-10-29 11:53:05 +0100

    modifications

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,3 +1,25 @@
 exports.adminPage = (req, res) => {
     res.render('admin/admin-dashboard', { title: 'Admin' })
 };
+
+exports.adminLogin = (req, res) => {
+    res.render('admin/admin-login', { title: 'Admin Login' })
+};
+
+exports.adminLoginAuth = (req, res) => {
+    let admin = {
+        username: req.body.uname,
+        password: req.body.pwd,
+    }
+    res.json({
+        message: 'logging in as admin'
+    })
+    // res.redir('/admin')
+}
+
+exports.lawyerDetails = (req, res) => {
+    res.json({
+        status: 200,
+        message: 'lawyer details'
+    })
+};
--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,33 @@
-var express = require('express');
-var router = express.Router();
-
+const express = require('express');
+const router = express.Router();
 const adminController = require('../controllers/adminController');
 
 router.get('/', adminController.adminPage);
+router.get('/login', adminController.adminLogin);
+
+// admin login route
+router.post('/auth', adminController.adminLoginAuth);
+// lawyer details route
+router.get('/lawyerDetails', adminController.lawyerDetails);
+
+
+// 
+// test create user page
+// 
+router.get('/newUser', (req, res) => {
+    res.render('admin/new-user')
+})
+
+router.post('/createUser', (req, res) => {
+    let newUser = {
+        username: req.body.uname,
+        password: req.body.pwd,
+    }
+
+    res.json({
+        status: 201,
+        message: 'Successful!'
+    })
+})
 
 module.exports = router;
