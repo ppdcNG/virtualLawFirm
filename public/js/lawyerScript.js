2aec0f35b36a5a82b802c6200ceb52a464958ec7
saslamp <sadiqasg@gmail.com>
2019-11-08 11:29:10 +0100

    mod

--- public/js/lawyerScript.js
+++ public/js/lawyerScript.js
@@ -24,25 +24,39 @@ $("#laywerConfirm").submit(function (e) {
   });
 });
 
-
 $("#lawyerLogin").submit(function (e) {
   e.preventDefault();
+  let form = form2js("lawyerLogin", ".");
 
-  var form = form2js("lawyerLogin", ".");
+  let { email, password } = form;
 
-  $.ajax({
-    url: ABS_PATH + "lawyer/lawyerLogin",
-    data: form,
-    type: "POST",
-    success: function (response) {
-      console.log(response);
-      if (!response.err) {
-        $.notify("User created successfully", { type: "success" });
-
-      } else {
-        $.notify(response.message, { type: "warning" });
-      }
-    },
-    error: e => console.log(e)
-  });
+  signIn(email, password);
 });
+var uservar;
+const signIn = async (email, password) => {
+  try {
+    let res = await firebase.auth().signInWithEmailAndPassword(email, password);
+    let uid = res.user.uid;
+    let idToken = await res.user.getIdToken();
+
+    let req = { idToken, uid };
+
+    let url = ABS_PATH + "lawyer/lawyerLogin";
+    $.ajax({
+      url: url,
+      data: req,
+      type: "POST",
+      success: function (response) {
+        console.log(response);
+        if (response.status == "success") {
+          $.notify("Logging in", { type: "success" });
+          setTimeout(function () { window.location = ABS_PATH + 'lawyer/home' }, 2000);
+        }
+      },
+      error: e => console.log(e)
+    });
+  } catch (e) {
+    console.log(e);
+    $.notify(e.message, { type: "danger" });
+  }
+};
