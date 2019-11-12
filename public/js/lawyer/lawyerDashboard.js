edd0cc5883cb16d240f9c8330317f230f6767cb2
saslamp <sadiqasg@gmail.com>
2019-11-12 14:44:26 +0100

    bootstrap notify path

--- public/js/lawyer/lawyerDashboard.js
+++ public/js/lawyer/lawyerDashboard.js
@@ -3,6 +3,20 @@ $("#lawyerContact").submit(function (e) {
 
     var form = form2js("lawyerContact", ".");
 
-    $.ajax()
+    $.ajax({
+        url: ABS_PATH + "lawyer/updateContact",
+        data: form,
+        type: "POST",
+        success: function (response) {
+            console.log(response);
+            if (!response.err) {
+                $.notify("User created successfully", { type: "success" });
+                setTimeout(function () { window.location = ABS_PATH + 'lawyer/login' }, 2000);
+            } else {
+                $.notify(response.message, { type: "warning" });
+            }
+        },
+        error: e => console.log(e)
+    });
 
 });
