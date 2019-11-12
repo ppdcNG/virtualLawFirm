be08118fb2f23b54dcbd2eb5fb50146c1cf5c3bd
saslamp <sadiqasg@gmail.com>
2019-11-12 15:16:13 +0100

    bug fix

--- public/js/lawyer/lawyerDashboard.js
+++ public/js/lawyer/lawyerDashboard.js
@@ -3,20 +3,7 @@ $("#lawyerContact").submit(function (e) {
 
     var form = form2js("lawyerContact", ".");
 
-    $.ajax({
-        url: ABS_PATH + "lawyer/updateContact",
-        data: form,
-        type: "POST",
-        success: function (response) {
-            console.log(response);
-            if (!response.err) {
-                $.notify("User created successfully", { type: "success" });
-                setTimeout(function () { window.location = ABS_PATH + 'lawyer/login' }, 2000);
-            } else {
-                $.notify(response.message, { type: "warning" });
-            }
-        },
-        error: e => console.log(e)
-    });
+    console.log('submitting..', form);
+
 
 });
