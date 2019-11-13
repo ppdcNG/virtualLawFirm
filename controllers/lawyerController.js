66a7093399987c1a2edc5c14f33994fe77187d97
saslamp <sadiqasg@gmail.com>
2019-11-13 14:09:48 +0100

    update record feature

--- controllers/lawyerController.js
+++ controllers/lawyerController.js
@@ -121,7 +121,9 @@ exports.updateContact = async (req, res) => {
 }
 
 exports.updateRecord = async (req, res) => {
-    let { courtEnrollmentNumber, yearOfEnrollment, criminalRecord, criminalInvestigation, misconductIndictment, misconductInvestigation, accountDetails } = req.body;
+    let data = JSON.parse(req.body.data);
+    let { courtEnrollmentNumber, yearOfEnrollment, criminalRecord, criminalInvestigation, misconductIndictment, misconductInvestigation, accountDetails } = data;
+    console.log(accountDetails);
     let user = await admin.firestore().collection('lawyers').doc(req.user.uid).get().catch((e) => {
         console.log(e);
         let returnObj = {
@@ -133,7 +135,6 @@ exports.updateRecord = async (req, res) => {
     });
     if (user.exists) {
         user = user.data();
-        await admin.auth().updateUser(req.user.uid, { photoUrl });
         user.record = { courtEnrollmentNumber, yearOfEnrollment, criminalRecord, criminalInvestigation, misconductIndictment, misconductInvestigation };
         user.accountDetails = accountDetails
         await admin.firestore().collection('lawyers').doc(req.user.uid).set(user);
