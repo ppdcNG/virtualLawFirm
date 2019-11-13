3908d63cf12df2b9467d95f8b29514ef067b3b71
unknown <victoradelowo@yahoo.com>
2019-11-13 15:45:44 +0100

    upload documents

--- public/js/lawyer/lawyerDashboard.js
+++ public/js/lawyer/lawyerDashboard.js
@@ -73,11 +73,37 @@ $("#updateRecord").submit(async function (e) {
 
 $("#selectDoc").submit(function (e) {
     e.preventDefault();
+    let doc = form2js("selectDoc", '.');
+    let uid = $("#uid").val();
+    let path = 'lawyerdocs/' + uid + '/' + doc.title;
+    let file = $("#docFile")[0].files[0];
+    let task = await firebase.storage().ref(path).put(file);
+    console.log(task);
+    let url = await task.ref.getDownloadURL();
+    doc.url = url;
+    documents.push(doc);
+    renderDocuments();
 
 });
 
 
-const renderDocument = document => {
+const renderDocument = (i, document) => {
 
+}
+
+const renderDocuments = () => {
+    let docHTML = '';
+    documents.map((doc, i) => {
+        docHTML += renderDocument(i, doc);
+    });
+    $("#uploadedDocs");
+}
+
+const removeDocument = i => {
+    let doc = documents[i];
+    let path = 'lawyerdocs/' + uid + '/' + doc.title;
+    await firebase.storage().ref(path).delete();
+    documents.splice(i, 1);
+    renderDocuments();
 }
 
