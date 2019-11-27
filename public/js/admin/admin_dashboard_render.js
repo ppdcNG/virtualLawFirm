fbae353e90d3356c88aa95dc5dc1d69f615d6649
saslamp <sadiqasg@gmail.com>
2019-11-27 11:56:44 +0100

    modify renderTable function

--- public/js/admin/admin_dashboard_render.js
+++ public/js/admin/admin_dashboard_render.js
@@ -1,10 +1,22 @@
 
 
-const renderLawyers = (lawyers)=>{
-// lawyers = [{ fullname, status, id.....}...]
+const renderLawyers = (lawyers) => {
+    // lawyers = [{ fullname, status, id.....}...]
 }
 
-const renderCases = (cases)=>{
+const renderCases = (cases) => {
 
 }
 
+const renderTable = (data) => {
+    return `
+        <tr>
+            <th scope="row">1</th>
+            <td>${data.fullName}</td>
+            <td>${data.status}</td>
+            <td>
+                <a onclick="" class="btn btn-sm text-dark" data-toggle="modal" data-target="#detailsModal">View</a>
+            </td>
+          </tr>
+        `
+}
