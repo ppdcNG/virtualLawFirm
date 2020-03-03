ff7057035ab27a70d85827791a60a8fcc029c64e
Abubakar Sambo <sadiqasg@gmail.com>
2020-03-03 16:26:47 +0100

    bug fixes

--- public/js/client/clientDashboard.js
+++ public/js/client/clientDashboard.js
@@ -245,7 +245,12 @@ const payWithPaystack = (fee, id) => {
             task.lawyer = laywer.contact;
             console.log(task);
 
-            console.log('laywer', laywer)
+            if (!task.lawyer.phoneNumber) {
+                task.lawyer.phoneNumber = '';
+            }
+            if (!task.lawyer.photoUrl) {
+                task.lawyer.photoUrl = '';
+            }
 
             let dataObj = {
                 paystackRef: response.reference,
@@ -257,25 +262,25 @@ const payWithPaystack = (fee, id) => {
 
             var processingNotification = $.notify('Processing payment, please wait', { type: "info", delay: 0 });
 
-            // $.ajax({
-            //     url: ABS_PATH + "client/verifyConsultationFee",
-            //     type: "POST",
-            //     data: req,
-            //     success: function (response) {
-            //         console.log("success", response);
+            $.ajax({
+                url: ABS_PATH + "client/verifyConsultationFee",
+                type: "POST",
+                data: req,
+                success: function (response) {
+                    console.log("success", response);
 
-            //         processingNotification.close();
-            //         $.notify(response.message, { type: response.status });
+                    processingNotification.close();
+                    $.notify(response.message, { type: response.status });
 
-            //         setTimeout(() => {
-            //             window.location = '/client/dashboard';
-            //         }, 1000)
-            //     },
-            //     error: err => {
-            //         console.error("error", err)
-            //         $.notify(response.message, { type: "warning" });
-            //     }
-            // });
+                    setTimeout(() => {
+                        window.location = '/client/dashboard';
+                    }, 1000)
+                },
+                error: err => {
+                    console.error("error", err)
+                    $.notify(response.message, { type: "warning" });
+                }
+            });
 
         },
         onClose: function () {
