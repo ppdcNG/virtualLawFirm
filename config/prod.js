3fbc9ae92786d5412f97145459e3fcbb53b447a2
unknown <victoradelowo@yahoo.com>
2020-03-03 13:05:47 +0100

    prodjs.modification

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,4 @@
-exports.SEND_GRID_API_KEY = process.ENV.SEND_GRID_API_KEY;
+exports.SEND_GRID_API_KEY = process.env.SEND_GRID_API_KEY;
 
 exports.ABS_PATH = 'https://lawtrella.herokuapp.com/';
 exports.AppName = 'LawTrella';
