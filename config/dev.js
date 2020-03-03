64fa7111544e6dab28614fc6b675907fabed44ef
Abubakar Sambo <sadiqasg@gmail.com>
2020-03-03 14:22:30 +0100

    removed config files

--- config/dev.js
+++
@@ -1,6 +0,0 @@
-exports.SEND_GRID_API_KEY =
-    "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";
-
-exports.ABS_PATH = 'http://localhost:3000/';
-exports.AppName = 'LawTrella';
-exports.PAYSTACK_PUB_KEY = "sk_test_8c562506dc0a6fa7340764b0493292a27b86a058";
