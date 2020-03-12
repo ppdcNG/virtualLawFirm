6340596ffa52adb9628572ab45ee518d75934074
Abubakar Sambo <sadiqasg@gmail.com>
2020-03-12 12:41:02 +0100

    bug fix

--- public/js/client/findLawyer.js
+++ public/js/client/findLawyer.js
@@ -60,7 +60,7 @@ const payWithPaystack = (fee, id) => {
 
 
     var handler = PaystackPop.setup({
-        key: "pk_test_28c944c0f505bdbe163c2d0083127cbaca3cb1c3",
+        key: PAYSTACK_KEY,
         email: clientEmail,
         amount: fee * 100,
         currency: "NGN",
