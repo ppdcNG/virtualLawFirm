e44062facf10d85f695122c8f567af1fe41df304
Abubakar Sambo <sadiqasg@gmail.com>
2020-03-19 15:38:04 +0100

    legal doc paystack

---
+++ public/js/legalDocs.js
@@ -0,0 +1,33 @@
+var pk = "pk_test_28c944c0f505bdbe163c2d0083127cbaca3cb1c3";
+
+const getLegalDoc = fee => {
+    // var amount = parseInt($("#amount").text());
+
+    (function payWithPaystack() {
+        var handler = PaystackPop.setup({
+            key: pk,
+            email: 'customer@email.com',
+            amount: amount * 100,
+            currency: "NGN",
+            ref: '' + Math.floor((Math.random() * 1000000000) + 1),
+            metadata: {
+                custom_fields: [
+                    {
+                        display_name: "Mobile Number",
+                        variable_name: "mobile_number",
+                        value: "+2348012345678"
+                    }
+                ]
+            },
+            callback: function (response) {
+                alert('success. transaction ref is ' + response.reference);
+            },
+            onClose: function () {
+                alert('window closed');
+            }
+        });
+        handler.openIframe();
+    })()
+
+}
+
