aac019f8fd391e40ff57e674ad41dba53a9c8256
Abubakar Sambo <sadiqasg@gmail.com>
2020-07-02 12:57:21 +0100

    feat: added paystack payment function to new consult card

--- public/js/client/findLawyer.js
+++ public/js/client/findLawyer.js
@@ -3,180 +3,67 @@ let lawyersList = {};
 var ISSUE = '1';
 let TASKS = {};
 
+async function handleFindLawyerSubmit(form, thisForm) {
+    $(thisForm).closest('.col-md-4').parent().css('display', 'none');
+    $("#loading").css('display', 'block');
+
+    let lawyers = await firebase.firestore().collection('lawyers').where('portfolio.tags', 'array-contains-any', form.tags).get().catch((e) => {
+        console.log(e);
+    });
+
+    let lawyersHTML = '';
+    lawyers.forEach(lawyer => {
+        let lawyerData = lawyer.data();
+        lawyersList[lawyer.id] = lawyerData;
+        lawyersHTML += foundMatchingLawyer(lawyer.data());
+    });
+
+    if (!lawyersHTML) {
+        lawyersHTML += "No Lawyers Found under the Category"
+    }
+    $("#loading").css('display', 'none');
+    $("#fetchMatchingLawyers").html(lawyersHTML);
+    $("#fetchMatchingLawyerSection").css('display', 'block');
+}
+
+// submit selected categories
 $("#categ--family-law").submit(async function (e) {
     e.preventDefault();
-    $(this).closest('.col-md-4').parent().css('display', 'none');
-    $("#loading").css('display', 'block');
-
     let form = form2js("categ--family-law", ".");
-
-    let lawyers = await firebase.firestore().collection('lawyers').where('portfolio.tags', 'array-contains-any', form.tags).get().catch((e) => {
-        console.log(e);
-    });
-
-    let lawyersHTML = '';
-    lawyers.forEach(lawyer => {
-        let lawyerData = lawyer.data();
-        lawyersList[lawyer.id] = lawyerData;
-        lawyersHTML += foundMatchingLawyer(lawyer.data());
-    });
-
-    if (!lawyersHTML) {
-        lawyersHTML += "No Lawyers Found under the Category"
-    }
-    $("#loading").css('display', 'none');
-    $("#fetchMatchingLawyers").html(lawyersHTML);
-    $("#fetchMatchingLawyerSection").css('display', 'block');
+    handleFindLawyerSubmit(form, this);
 })
+
 $("#categ--adminPublicLaw").submit(async function (e) {
     e.preventDefault();
-    $(this).closest('.col-md-4').parent().css('display', 'none');
-    $("#loading").css('display', 'block');
-
     let form = form2js("categ--adminPublicLaw", ".");
-
-    let lawyers = await firebase.firestore().collection('lawyers').where('portfolio.tags', 'array-contains-any', form.tags).get().catch((e) => {
-        console.log(e);
-    });
-
-    let lawyersHTML = '';
-    lawyers.forEach(lawyer => {
-        let lawyerData = lawyer.data();
-        lawyersList[lawyer.id] = lawyerData;
-        lawyersHTML += foundMatchingLawyer(lawyer.data());
-    });
-
-    if (!lawyersHTML) {
-        lawyersHTML += "No Lawyers Found under the Category"
-    }
-    $("#loading").css('display', 'none');
-    $("#fetchMatchingLawyers").html(lawyersHTML);
-    $("#fetchMatchingLawyerSection").css('display', 'block');
+    handleFindLawyerSubmit(form, this);
 });
 $("#categ--landProperty").submit(async function (e) {
     e.preventDefault();
-    $(this).closest('.col-md-4').parent().css('display', 'none');
-    $("#loading").css('display', 'block');
     let form = form2js("categ--landProperty", ".");
-
-    let lawyers = await firebase.firestore().collection('lawyers').where('portfolio.tags', 'array-contains-any', form.tags).get().catch((e) => {
-        console.log(e);
-    });
-
-    let lawyersHTML = '';
-    lawyers.forEach(lawyer => {
-        let lawyerData = lawyer.data();
-        lawyersList[lawyer.id] = lawyerData;
-        lawyersHTML += foundMatchingLawyer(lawyer.data());
-    });
-
-    if (!lawyersHTML) {
-        lawyersHTML += "No Lawyers Found under the Category"
-    }
-    $("#loading").css('display', 'none');
-    $("#fetchMatchingLawyers").html(lawyersHTML);
-    $("#fetchMatchingLawyerSection").css('display', 'block');
+    handleFindLawyerSubmit(form, this);
 });
 $("#categ--financeCommercial").submit(async function (e) {
     e.preventDefault();
-    $(this).closest('.col-md-4').parent().css('display', 'none');
-    $("#loading").css('display', 'block');
-
     let form = form2js("categ--financeCommercial", ".");
-
-    let lawyers = await firebase.firestore().collection('lawyers').where('portfolio.tags', 'array-contains-any', form.tags).get().catch((e) => {
-        console.log(e);
-    });
-
-    let lawyersHTML = '';
-    lawyers.forEach(lawyer => {
-        let lawyerData = lawyer.data();
-        lawyersList[lawyer.id] = lawyerData;
-        lawyersHTML += foundMatchingLawyer(lawyer.data());
-    });
-
-    if (!lawyersHTML) {
-        lawyersHTML += "No Lawyers Found under the Category"
-    }
-    $("#loading").css('display', 'none');
-    $("#fetchMatchingLawyers").html(lawyersHTML);
-    $("#fetchMatchingLawyerSection").css('display', 'block');
+    handleFindLawyerSubmit(form, this);
 });
 $("#categ--digitalEntertainmentSport").submit(async function (e) {
     e.preventDefault();
-    $(this).closest('.col-md-4').parent().css('display', 'none');
-    $("#loading").css('display', 'block');
-
     let form = form2js("categ--digitalEntertainmentSport", ".");
-
-    let lawyers = await firebase.firestore().collection('lawyers').where('portfolio.tags', 'array-contains-any', form.tags).get().catch((e) => {
-        console.log(e);
-    });
-
-    let lawyersHTML = '';
-    lawyers.forEach(lawyer => {
-        let lawyerData = lawyer.data();
-        lawyersList[lawyer.id] = lawyerData;
-        lawyersHTML += foundMatchingLawyer(lawyer.data());
-    });
-
-    if (!lawyersHTML) {
-        lawyersHTML += "No Lawyers Found under the Category"
-    }
-    $("#loading").css('display', 'none');
-    $("#fetchMatchingLawyers").html(lawyersHTML);
-    $("#fetchMatchingLawyerSection").css('display', 'block');
+    handleFindLawyerSubmit(form, this);
 });
 $("#categ--energyProjects").submit(async function (e) {
     e.preventDefault();
-    $(this).closest('.col-md-4').parent().css('display', 'none');
-    $("#loading").css('display', 'block');
-
     let form = form2js("categ--energyProjects", ".");
-
-    let lawyers = await firebase.firestore().collection('lawyers').where('portfolio.tags', 'array-contains-any', form.tags).get().catch((e) => {
-        console.log(e);
-    });
-
-    let lawyersHTML = '';
-    lawyers.forEach(lawyer => {
-        let lawyerData = lawyer.data();
-        lawyersList[lawyer.id] = lawyerData;
-        lawyersHTML += foundMatchingLawyer(lawyer.data());
-    });
-
-    if (!lawyersHTML) {
-        lawyersHTML += "No Lawyers Found under the Category"
-    }
-    $("#loading").css('display', 'none');
-    $("#fetchMatchingLawyers").html(lawyersHTML);
-    $("#fetchMatchingLawyerSection").css('display', 'block');
+    handleFindLawyerSubmit(form, this);
 });
 $("#categ--others").submit(async function (e) {
     e.preventDefault();
-    $(this).closest('.col-md-4').parent().css('display', 'none');
-    $("#loading").css('display', 'block');
-
     let form = form2js("categ--others", ".");
-
-    let lawyers = await firebase.firestore().collection('lawyers').where('portfolio.tags', 'array-contains-any', form.tags).get().catch((e) => {
-        console.log(e);
-    });
-
-    let lawyersHTML = '';
-    lawyers.forEach(lawyer => {
-        let lawyerData = lawyer.data();
-        lawyersList[lawyer.id] = lawyerData;
-        lawyersHTML += foundMatchingLawyer(lawyer.data());
-    });
-
-    if (!lawyersHTML) {
-        lawyersHTML += "No Lawyers Found under the Category"
-    }
-    $("#loading").css('display', 'none');
-    $("#fetchMatchingLawyers").html(lawyersHTML);
-    $("#fetchMatchingLawyerSection").css('display', 'block');
+    handleFindLawyerSubmit(form, this);
 });
+// submit select categories end 
 
 const foundMatchingLawyer = lawyer => {
     let { contact, portfolio, name, authId } = lawyer;
@@ -192,7 +79,7 @@ const foundMatchingLawyer = lawyer => {
                                 alt="avatar image" height="80" width="80">
                         </div>
                         <div class="d-flex justify-content-between">
-                            <button class="btn btn-warning btn-sm">${portfolio.specialization}</button><button class="btn btn-warning btn-sm"></button><button class="btn btn-warning btn-sm"></button>
+                            <button class="btn btn-warning btn-sm">${portfolio.specialization}</button><button class="btn btn-warning btn-sm"></button><button class="btn btn-warning btn-sm" onclick="payWithPaystack('${portfolio.consultationFee}', '${authId}')">Consult</button>
                         </div>
                     </div>
                     <div class="card-footer d-flex justify-content-between">
@@ -207,54 +94,54 @@ $("#backToFindLawyer").click(function () {
     location.reload();
 });
 
-$("#findLawyerForm").submit(async function (e) {
-    e.preventDefault();
+// $("#findLawyerForm").submit(async function (e) {
+//     e.preventDefault();
 
-    let form = form2js("findLawyerForm", ".");
-    console.log(form);
-    ISSUE = JSON.stringify(form);
+//     let form = form2js("findLawyerForm", ".");
+//     console.log(form);
+//     ISSUE = JSON.stringify(form);
 
-    // $.notify(response.message, {type: "Searching Lawyers.." });
+//     // $.notify(response.message, {type: "Searching Lawyers.." });
 
-    // form = JSON.stringify(form);
+//     // form = JSON.stringify(form);
 
-    // let data = {data: form }
-    // console.log(data);
-    let data = {};
-    let lawyers = await firebase.firestore().collection('lawyers').where('portfolio.tags', 'array-contains-any', form.tags).get().catch((e) => {
-        console.log(e);
-    });
-    console.log('firbase done')
-    console.log(lawyers);
-    let lawyersHTML = '';
-    lawyers.forEach(lawyer => {
-        let lawyerData = lawyer.data();
-        lawyersList[lawyer.id] = lawyerData;
-        lawyersHTML += renderFoundLawyer(lawyer.data());
-    });
-    $("#fetchlawyersList").html(lawyersHTML);
-    $("#findLawyersSection").css('display', 'none');
-    $("#fetchLawyersSection").css('display', 'block');
-    clearLoad("next", "Next");
-});
-$("#prev").click(async function (e) {
-    $("#fetchLawyersSection").css('display', 'none');
-    $("#findLawyersSection").css('display', 'block');
-});
-const renderFoundLawyer = lawyer => {
-    let { contact, portfolio, name, authId } = lawyer;
-    let fee = accounting.formatNumber(portfolio.consultationFee);
+//     // let data = {data: form }
+//     // console.log(data);
+//     let data = {};
+//     let lawyers = await firebase.firestore().collection('lawyers').where('portfolio.tags', 'array-contains-any', form.tags).get().catch((e) => {
+//         console.log(e);
+//     });
+//     console.log('firbase done')
+//     console.log(lawyers);
+//     let lawyersHTML = '';
+//     lawyers.forEach(lawyer => {
+//         let lawyerData = lawyer.data();
+//         lawyersList[lawyer.id] = lawyerData;
+//         lawyersHTML += renderFoundLawyer(lawyer.data());
+//     });
+//     $("#fetchlawyersList").html(lawyersHTML);
+//     $("#findLawyersSection").css('display', 'none');
+//     $("#fetchLawyersSection").css('display', 'block');
+//     clearLoad("next", "Next");
+// });
+// $("#prev").click(async function (e) {
+//     $("#fetchLawyersSection").css('display', 'none');
+//     $("#findLawyersSection").css('display', 'block');
+// });
+// const renderFoundLawyer = lawyer => {
+//     let { contact, portfolio, name, authId } = lawyer;
+//     let fee = accounting.formatNumber(portfolio.consultationFee);
 
-    return `<li class="list-group-item d-flex justify-content-between align-items-center">
-        <img src="${contact.photoUrl}" class="rounded-circle mr-1" alt="profile_pic" width="40" />
-        <span class="flex-fill">${name}</span><br />
-        <span class="flex-fill"><b>Specialization: </b>${portfolio.specialization}</span>
-        <span class="flex-fill"><b>Experience: ${portfolio.workExperience} Years</b></span>
-        <span class="badge badge-info badge-pill p-3" style="width:100px;">&#8358;<span style="font-size:larger">${fee}</span></span>
-        <a class="btn blue-text ml-4" onclick="payWithPaystack('${portfolio.consultationFee}', '${authId}')">Consult</a>
-    </li>
-`
-}
+//     return `<li class="list-group-item d-flex justify-content-between align-items-center">
+//         <img src="${contact.photoUrl}" class="rounded-circle mr-1" alt="profile_pic" width="40" />
+//         <span class="flex-fill">${name}</span><br />
+//         <span class="flex-fill"><b>Specialization: </b>${portfolio.specialization}</span>
+//         <span class="flex-fill"><b>Experience: ${portfolio.workExperience} Years</b></span>
+//         <span class="badge badge-info badge-pill p-3" style="width:100px;">&#8358;<span style="font-size:larger">${fee}</span></span>
+//         <a class="btn blue-text ml-4" onclick="payWithPaystack('${portfolio.consultationFee}', '${authId}')">Consult</a>
+//     </li>
+// `
+// }
 
 // Paystack
 const payWithPaystack = (fee, id) => {
