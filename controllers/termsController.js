fb25c556522ed4e7aab25e526979dff526ec1d58
Abubakar Sambo <sadiqasg@gmail.com>
2020-07-09 10:52:22 +0100

    feat: added lawyer terms of use

--- controllers/termsController.js
+++ controllers/termsController.js
@@ -7,3 +7,7 @@ exports.privacyPolicy = (req, res) => {
 exports.termsOfUse = (req, res) => {
     res.render('terms/terms-ofUse', { title: 'Terms Of Use', AppName });
 };
+
+exports.lawyerTerms = (req, res) => {
+    res.render('terms/lawyer-terms', { title: 'Lawyer Terms Of Use', AppName });
+};
--- routes/terms.js
+++ routes/terms.js
@@ -5,5 +5,6 @@ const termsController = require('../controllers/termsController');
 
 router.get('/privacy', termsController.privacyPolicy);
 router.get('/terms', termsController.termsOfUse);
+router.get('/lawyerTerms', termsController.lawyerTerms);
 
 module.exports = router;
