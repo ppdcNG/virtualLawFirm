9f6e78bd2a6f652282b5df1a6ae9832926c126dc
unknown <victoradelowo@yahoo.com>
2020-08-03 15:35:02 +0100

    hhtps url config

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,6 @@
 exports.SEND_GRID_API_KEY = process.env.SEND_GRID_API_KEY;
 
-exports.ABS_PATH = 'http://www.lawtrella.com/';
+exports.ABS_PATH = 'https://www.lawtrella.com/';
 exports.AppName = 'LawTrella';
 exports.PAYSTACK_PUB_KEY = process.env.PAYSTACK_PUB_KEY;
 exports.FIREBASE_STORAGE_BUCKET = process.env.FIREBASE_STORAGE_BUCKET;
