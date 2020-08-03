66c362e35d35b4ddbea20138de3459c0744a7a34
Abubakar Sambo <sadiqasg@gmail.com>
2020-08-03 19:17:10 +0100

    feat: added new categories

--- config/categories/index.js
+++ config/categories/index.js
@@ -1,15 +1,45 @@
 
+// const familyLaw = require('./family-law.json');
+// const administrativePublicLaw = require('./administrative-public-law.json');
+// const landPropertyLaw = require('./land-property-law.json');
+// const financeCommercialLaw = require('./finance-commercial-law.json');
+// const digitalEntertainmentLaw = require('./digital-ent-sports.json');
+// const energyProjectsLaw = require('./energy-projects.json');
+// const others = require('./others.json');
 
-const familyLaw = require('./family-law.json');
-const administrativePublicLaw = require('./administrative-public-law.json');
-const landPropertyLaw = require('./land-property-law.json');
-const financeCommercialLaw = require('./finance-commercial-law.json');
-const digitalEntertainmentLaw = require('./digital-ent-sports.json');
-const energyProjectsLaw = require('./energy-projects.json');
-const others = require('./others.json');
-
-
+// new categories
+const agriculturalLaw = require('./agricultural-law.json');
+const antitrustSection = require('./antitrust-section.json');
+const corporateLawSection = require('./corporate-law-section.json');
+const disputeResolutionSection = require('./dispute-resolution.json');
+const EENRILawSection = require('./eenri-law-section.json'); //Energy, Environment, Natural Resources andInfrastructure Law Section
+const financialServicesSection = require('./financial-services.json');
+const humanResourcesSection = require('./human-resources.json');
+const insolvency = require('./insolvency.json');
+const IPCTSection = require('./ipct-section.json'); //Intellectual Property, Communications and Technology Section
+const ICTFPLawSection = require('./ictfp-law-section.json'); //Intellectual Commercial, Trade, Franchising and Product Law Section
+const lawAndIndiviualRights = require('./law-indiviual-rights.json');
+const leisureIndustriesSection = require('./leisure-industries.json');
+const maritimeAndAviationLaw = require('./maritime-aviation-law.json');
+const realEstateSection = require('./real-estate.json');
+const publicLawSection = require('./public-law-section.json');
+const taxationSection = require('./taxation-section.json');
 
 module.exports = {
-    familyLaw, administrativePublicLaw, landPropertyLaw, financeCommercialLaw, digitalEntertainmentLaw, energyProjectsLaw, others
+    agriculturalLaw,
+    antitrustSection,
+    corporateLawSection,
+    disputeResolutionSection,
+    EENRILawSection,
+    financialServicesSection,
+    humanResourcesSection,
+    insolvency,
+    IPCTSection,
+    ICTFPLawSection,
+    lawAndIndiviualRights,
+    leisureIndustriesSection,
+    maritimeAndAviationLaw,
+    realEstateSection,
+    publicLawSection,
+    taxationSection
 }
