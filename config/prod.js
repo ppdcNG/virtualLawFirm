78ba2f87c65eb149c37c96ac0b83f4b4fcc93456
unknown <victoradelowo@yahoo.com>
2020-08-18 08:53:36 +0100

    prod keys

--- config/prod.js
+++ config/prod.js
@@ -4,3 +4,5 @@ exports.ABS_PATH = 'https://www.lawtrella.com/';
 exports.AppName = 'LawTrella';
 exports.PAYSTACK_PUB_KEY = process.env.PAYSTACK_PUB_KEY;
 exports.FIREBASE_STORAGE_BUCKET = process.env.FIREBASE_STORAGE_BUCKET;
+exports.MAILGUN_API_KEY = process.env.MAILGUN_API_KEY;
+exports.MAILGUN_DOMAIN = process.env.MAILGUN_DOMAIN;
